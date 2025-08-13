7ae9f2c0c11d76aff24910fe82faf0396d1ceac7
Roman Blazej <blazejroman14@gmail.com>
2025-08-13 16:46:40 +0200

    test01

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+  return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -100,4 +105,5 @@ export default function Home() {
       </footer>
     </div>
   );
+  
 }
